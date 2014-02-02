89d20fdafb8e6c958dd5bfda471e9a0c8f925b47
75py <dev.75py@gmail.com>
2014-02-02 15:09:38 +0900

    トースト アイコン表示時のテキストの表示位置を調整

--- src/com/nagopy/android/xposed/utilities/receiver/ToastReceiver.java
+++ src/com/nagopy/android/xposed/utilities/receiver/ToastReceiver.java
@@ -24,6 +24,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.preference.PreferenceManager;
+import android.view.Gravity;
 import android.widget.TextView;
 import android.widget.Toast;
 
